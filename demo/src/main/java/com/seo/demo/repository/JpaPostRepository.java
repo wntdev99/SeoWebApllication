94a4e0c26666c2e15d5b10910b7a2bc7461f6096
wntdev99 <wntdev99@gmail.com>
2024-02-08 02:35:16 +0900

    [Add] JPA 로직 구현

---
+++ demo/src/main/java/com/seo/demo/repository/JpaPostRepository.java
@@ -0,0 +1,38 @@
+package com.seo.demo.repository;
+
+import com.seo.demo.domain.Post;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaPostRepository implements PostRepository{
+
+    private final EntityManager em;
+
+    @Autowired
+    public JpaPostRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Post savePost(Post post) {
+        em.persist(post);
+        return post;
+    }
+
+    @Override
+    public Optional<Post> loadPost(String url) {
+        List<Post> result = em.createQuery("select p from Post p where p.url = url", Post.class)
+                .setParameter("url", url)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Post> loadAllPost() {
+        return em.createQuery("select p from Post as p", Post.class)
+                .getResultList();
+    }
+}
